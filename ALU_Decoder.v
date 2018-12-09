e329fd8506692950184202bdbfdf03d4fb5e85d7
giorgianb <giorgian.borca-tasciuc@protonmail.com>
2018-12-08 19:06:10 -0500

    Moved test bench to seperate file

--- ALU_Decoder.v
+++ ALU_Decoder.v
@@ -23,87 +23,21 @@ reg [2:0] ALUControl;
 
 always @*
 begin
+//	$display("ALUOp: %b\tFunct: %b", ALUOp, Funct);
 	case (ALUOp)
-		`ALU_DECODER_ADD: ALUControl <= `ALU_ADD;
-		`ALU_DECODER_SUBTRACT: ALUControl <= `ALU_SUBTRACT;
+		`ALU_DECODER_ADD: ALUControl = `ALU_ADD;
+		`ALU_DECODER_SUBTRACT: ALUControl = `ALU_SUBTRACT;
 		`ALU_DECODER_FUNCT:	// Look at funct [3:0] (first two bits are always 10)
 			case (Funct[3:0])
-				`FUNCT_ADD: ALUControl <= `ALU_ADD;
-				`FUNCT_SUBTRACT: ALUControl <= `ALU_SUBTRACT;
-				`FUNCT_AND: ALUControl <= `ALU_AND;
-				`FUNCT_OR: ALUControl <= `ALU_OR;
-				`FUNCT_SLT: ALUControl <= `ALU_SLT;
-				default: $display("Unknown Funct: %g", Funct);
+				`FUNCT_ADD: ALUControl = `ALU_ADD;
+				`FUNCT_SUBTRACT: ALUControl = `ALU_SUBTRACT;
+				`FUNCT_AND: ALUControl = `ALU_AND;
+				`FUNCT_OR: ALUControl = `ALU_OR;
+				`FUNCT_SLT: ALUControl = `ALU_SLT;
+				default: $display("Unknown Funct: %b", Funct);
 			endcase
-		default: $display("Unknown ALUOp: %g", ALUOp);
+		default: $display("Unknown ALUOp: %b", ALUOp);
 	endcase
-end
-endmodule
-
-module ALU_Decoder_Test();
-
-reg CLK;
-reg [1:0] ALUOp;
-reg [5:0] Funct;
-reg [2:0] ALUControl;
-
-initial
-begin
-	ALUOp <= `ALU_DECODER_ADD;
-	Funct <= 6'bxxxxxx;
-	if (ALUControl != `ALU_ADD)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_ADD, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_SUBTRACT;
-	Funct <= 6'bxxxxxx;
-	if (ALUControl != `ALU_SUBTRACT)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_SUBTRACT, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_FUNCT;
-	Funct <= `FUNCT_ADD;
-	if (ALUControl != `ALU_ADD)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_ADD, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_FUNCT;
-	Funct <= `FUNCT_SUBTRACT;
-	if (ALUControl != `ALU_SUBTRACT)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_SUBTRACT, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_FUNCT;
-	Funct <= `FUNCT_AND;
-	if (ALUControl != `ALU_AND)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_AND, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_FUNCT;
-	Funct <= `FUNCT_OR;
-	if (ALUControl != `ALU_OR)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_OR, ALUControl);
-		$finish;
-	end
-
-	ALUOp <= `ALU_DECODER_FUNCT;
-	Funct <= `FUNCT_SLT;
-	if (ALUControl != `ALU_SLT)
-	begin
-		$display("Expected: %g\tReceived: %g", `ALU_SLT, ALUControl);
-		$finish;
-	end
 end
 endmodule
 `endif
